55a71a2a3f1ad4c27150f0e6d1bdf5dd7589bfb1
Ron788 <ustinov200511@gmail.com>
2021-06-24 16:22:23 +0300

    init

---
+++ ASCII Converter/ASCII Converter/Converter/Convert.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+
+namespace ASCII_Converter.Converter
+{
+    public class ConvertFile
+    {
+        private string _path; 
+        public ConvertFile(string path)
+        {
+            _path = path;
+        }
+
+        public void Start()
+        {
+            Bitmap bitmap = new Bitmap(_path);
+            bitmap.ToGrayscale();
+
+            var converter = new BitmapToASCII(bitmap);
+            var rows = converter.Convert();
+
+            File.WriteAllLines("text.txt", rows.Select(r => new string(r)));
+        }
+    }
+}
---
+++ ASCII Converter/ASCII Converter/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ASCII_Converter.Converter;
+
+namespace ASCII_Converter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Image | *.png; *.jpg"
+            };
+
+            if (openFileDialog.ShowDialog() != DialogResult.OK) return;
+
+            ConvertFile convert = new ConvertFile(openFileDialog.FileName);
+            convert.Start();
+        }
+    }
+}
